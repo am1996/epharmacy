262fe3d2212049b7650f3dce46bb18a59fdf1834
Ahmed Magdy <ahmedmagdy92@yahoo.com>
2024-11-12 08:34:47 +0200

    LoginView implemented and LoginForm implemented

--- users/views.py
+++ users/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
-from django.http import HttpResponse
-
+from django.contrib.auth import authenticate,login
+from .forms import LoginForm
 # Create your views here.
 
 class UserLoginView(View):
     def get(self,request):
-        return HttpResponse("hello world")
+        return render(request,"login.html",{"login_form":LoginForm})
+
+    def post(self,request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+
+        else:
+            form = LoginForm()
+
+        return render(request, 'login.html', {'form': form})
